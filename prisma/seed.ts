4877f6e22954593ebfc7093c07ca04cb732d0801
Ed Dixon <ed@Eds-MacBook-Air.local>
2026-08-03 22:30:38 +0100

    add seed db script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 
-import { PrismaClient, Status, Gym, CompetitionType, Competition, CompetitionCategory, CompetitionProblem } from "@prisma/client";
+import { PrismaClient, Status, StaffRole, Result, Gym, CompetitionType, Attempt, Competition, CompetitionCategory,CompetitionEntry, GymStaff, CompetitionProblem, User } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -11,6 +11,10 @@ async function main() {
   const competitions = await seedCompetitions(gyms, competitionTypes);
   const competitionCategories = await seedCompetitionCategories(competitions);
   const competitionProblems = await seedProblems(competitions);
+  const users = await seedUsers();
+  const gymStaff = await seedGymStaff(gyms, users);
+  const competitionEntries = await seedCompetitionEntries(competitions, users, competitionCategories);
+  const attempts = await seedAttempts(competitionEntries, competitionProblems);
   console.log("✅ Database seeded!");
 }
 
@@ -189,6 +193,143 @@ async function seedProblems(competitions: Competition[]) {
         createdProblems.push(createdProblem);
     }
     return createdProblems;
+}
+
+async function seedUsers() {
+    const users = [{
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@example.com",
+        passwordHash: "hashedpassword",
+    },
+    {   firstName: "Jane",
+        lastName: "Smith",
+        email: "jane.smith@example.com",
+        passwordHash: "hashedpassword",
+    },
+    {   firstName: "Alice",
+        lastName: "Johnson",
+        email: "alice.johnson@example.com",
+        passwordHash: "hashedpassword",
+    },
+    {   firstName: "Bob",
+        lastName: "Brown",
+        email: "bob.brown@example.com",
+        passwordHash: "hashedpassword",
+    },
+    {   firstName: "Charlie",
+        lastName: "Davis",
+        email: "charlie.davis@example.com",
+        passwordHash: "hashedpassword",
+    }
+    ]
+
+    const createdUsers: User[] = [];
+
+    for (const user of users) {
+        const createdUser = await prisma.user.upsert({
+            where: { email: user.email },
+            update: {},
+            create: user,
+        });
+        createdUsers.push(createdUser);
+    }
+    return createdUsers;
+}
+
+async function seedGymStaff(gyms: Gym[], users: User[]) {
+    const gymStaff = [{
+        gymId: gyms[0].id,
+        userId: users[0].id,
+        role: StaffRole.ADMIN
+    },
+    {
+        gymId: gyms[0].id,
+        userId: users[1].id,
+        role: StaffRole.STAFF
+    },]
+
+    const createdGymStaff: GymStaff[] = [];
+
+    for (const staff of gymStaff) {
+        const createdStaff = await prisma.gymStaff.upsert({
+            where: { gymId_userId: { gymId: staff.gymId, userId: staff.userId } },
+            update: {},
+            create: staff,
+        });
+        createdGymStaff.push(createdStaff);
+    }
+    return createdGymStaff;
+}
+
+async function seedCompetitionEntries(competitions: Competition[], users: User[], competitionCategories: CompetitionCategory[]) {
+    const entries = [
+        {competitionId: competitions[0].id, userId: users[0].id, categoryId: competitionCategories[0].id},
+        {competitionId: competitions[0].id, userId: users[1].id, categoryId: competitionCategories[1].id},
+        {competitionId: competitions[0].id, userId: users[2].id, categoryId: competitionCategories[2].id},
+        {competitionId: competitions[0].id, userId: users[3].id, categoryId: competitionCategories[3].id},
+    ];
+
+    const createdEntries: CompetitionEntry[] = [];
+
+    for (const entry of entries) {
+        const createdEntry = await prisma.competitionEntry.upsert({
+            where: { competitionId_userId: { competitionId: entry.competitionId, userId: entry.userId } },
+            update: {},
+            create: entry,
+        });
+        createdEntries.push(createdEntry);
+    }
+    return createdEntries;
+}
+
+async function seedAttempts(competitionEntries: CompetitionEntry[], competitionProblems: CompetitionProblem[]) {
+    const attempts = [
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 1, result: Result.TOP },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 1, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 2, result: Result.TOP },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 2, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 3, result: Result.TOP },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 1, result: Result.TOP },
+  { competitionEntryId: competitionEntries[0].id, competitionProblemId: competitionProblems[4].id, attemptNumber: 1, result: Result.NONE },
+
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 1, result: Result.TOP },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 1, result: Result.TOP },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 1, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 2, result: Result.TOP },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 2, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[1].id, competitionProblemId: competitionProblems[4].id, attemptNumber: 1, result: Result.NONE },
+
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 1, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 2, result: Result.TOP },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 2, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 1, result: Result.TOP },
+  { competitionEntryId: competitionEntries[2].id, competitionProblemId: competitionProblems[4].id, attemptNumber: 1, result: Result.ZONE },
+
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[0].id, attemptNumber: 2, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[1].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[2].id, attemptNumber: 1, result: Result.NONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 1, result: Result.ZONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[3].id, attemptNumber: 2, result: Result.NONE },
+  { competitionEntryId: competitionEntries[3].id, competitionProblemId: competitionProblems[4].id, attemptNumber: 1, result: Result.NONE },
+]
+
+const createdAttempts: Attempt[] = [];
+
+for (const attempt of attempts) {
+    const createdAttempt = await prisma.attempt.upsert({
+        where: { competitionEntryId_competitionProblemId_attemptNumber: { competitionEntryId: attempt.competitionEntryId, competitionProblemId: attempt.competitionProblemId, attemptNumber: attempt.attemptNumber } },
+        update: {},
+        create: attempt,
+    });
+    createdAttempts.push(createdAttempt);
+}
+return createdAttempts;
 }
 
 main()
